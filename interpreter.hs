f3dac9399d5c2fd6a9ff606157ed0943d3d43e42
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2017-06-15 03:35:21 +0200

    Implemented Label-function for noting jump-locations Added support for use of labels troughout the program (to accomodate Jumps) by preprocessing the labels Implemented Jmp-function for jumping trough the program Implemented Cmp-function for comparing values and saving the result in a register Implemented Add-function for adding two values and writing the sum in a register Defined and applied common types for simplifying function signatures

--- interpreter.hs
+++ interpreter.hs
@@ -10,6 +10,19 @@ import System.Environment
 
 
 
+-- Additional Types
+
+-- list of Labels, each label has a string, list of instructions and the program counter
+type Labels = [(String, [Instruction], Int)]
+
+-- Tuple of remaining program, memory, registery, program counter and labels
+type InstructionArgs = ([Instruction], [Literal], [Literal], Int, Labels)
+type InstructionResult = ([Instruction], [Literal], [Literal], Int, Labels)
+
+
+
+
+
 -- Main Function
 
 -- reads arguments and the SVM-file, initializes memory and registers and starts execution unless Parse Errors
@@ -22,12 +35,13 @@ main = do
   -- initializing the memory of size N where N is the command line argument
   let memorysize = (read (memsize!!0) :: Int)
   let memory = initMem memorysize
+  -- initializing the register
   let registers = initReg
 
   -- unwrap Either: prog of type Program or err of type ParseError
   case result of
     Left err -> printError err
-    Right prog -> run prog memory registers 1 -- use "run" or "debug"
+    Right prog -> run (prog, memory, registers, 1, (prerun (prog, memory, registers, 1, []))) -- use "run" or "debug"
 
 
 
@@ -35,22 +49,28 @@ main = do
 
 -- Run Function
 
+-- recursive function for preprocessing the list of labels
+prerun :: InstructionArgs -> Labels
+prerun ([], mem, reg, pc, labels) = labels
+prerun (((Label string):list), mem, reg, pc, labels) = prerun (list, mem, reg, (pc+1), (labels ++ [(string, list, pc)]))
+prerun ((instruction:list), mem, reg, pc, labels) = prerun (list, mem, reg, (pc+1), labels)
+
 -- recursive function for running the program
-run :: [Instruction] -> [Literal] -> [Literal] -> Int -> IO()
-run [] mem reg pc = do
+run :: InstructionArgs -> IO()
+run ([], mem, reg, pc, labels) = do
   putStrLn ("End of program at PC " ++ (show pc))
   prettyPrintMem mem False
-run (instruction:list) mem reg pc = do
+run ((instruction:list), mem, reg, pc, labels) = do
   putStrLn "Current Storage State:"
   prettyPrintReg reg True
   prettyPrintMem mem False
   putStrLn "\n\n\n"
   putStrLn (multiplyCharRec '-' "" 0 100)
   putStrLn ("\n\nInstruction '" ++ (show instruction) ++ "'")
-  putStrLn ("ProgramCounter " ++ (show pc))  -- displays the programcounter
-  case (execute instruction mem reg) of
-    Left err -> putStrLn err                 -- if error, halt program and report
-    Right (rmem, rreg) -> run list rmem rreg (pc+1) -- if result, go on with recursion
+  putStrLn ("ProgramCounter " ++ (show pc))                                           -- displays the programcounter
+  case (execute instruction (list, mem, reg, pc, labels)) of
+    Left err -> putStrLn err                                                          -- if error, halt program and report
+    Right (rlist, rmem, rreg, rpc, rlabels) -> run (rlist, rmem, rreg, rpc, rlabels)  -- if result, go on with recursion
 
 
 
@@ -60,25 +80,25 @@ run (instruction:list) mem reg pc = do
 -- Execute Statement Function
 
 -- function for pattern matching and executing an instruction, example: "execute instruction mem"
-execute :: Instruction -> [Literal] -> [Literal] -> Either error ([Literal], [Literal])
+execute :: Instruction -> InstructionArgs -> Either error InstructionResult
 -- /func /mem /name /arg1 /arg2 /statement
-execute (Nop)               mem reg = nop mem reg
-execute (Mov    loc val)    mem reg = mov loc val mem reg
--- execute (And    reg val)    mem reg = print reg
--- execute (Or     reg val)    mem reg = print reg
--- execute (Not    reg)        mem reg = print reg
--- execute (Mod    reg val)    mem reg = print reg
--- execute (Add    reg val)    mem reg = print reg
--- execute (Sub    reg val)    mem reg = print reg
--- execute (Mul    reg val)    mem reg = print reg
--- execute (Div    reg val)    mem reg = print reg
--- execute (Cmp    reg val)    mem reg = print reg
--- execute (Label  string)     mem reg = print string
--- execute (Jmp    string)     mem reg = print string
--- execute (Jc     string reg) mem reg = print string
--- execute (Jeq    string reg) mem reg = print string
+execute (Nop)               (list, mem, reg, pc, labels) = nop (list, mem, reg, pc, labels)
+execute (Mov    loc val)    (list, mem, reg, pc, labels) = mov loc val (list, mem, reg, pc, labels)
+-- execute (And    rg val)    (list, mem, reg, pc, labels) = print reg
+-- execute (Or     rg val)    (list, mem, reg, pc, labels) = print reg
+-- execute (Not    rg)        (list, mem, reg, pc, labels) = print reg
+-- execute (Mod    rg val)    (list, mem, reg, pc, labels) = print reg
+execute (Add    rg val)    (list, mem, reg, pc, labels) = add rg val (list, mem, reg, pc, labels)
+-- execute (Sub    rg val)    (list, mem, reg, pc, labels) = print reg
+-- execute (Mul    rg val)    (list, mem, reg, pc, labels) = print reg
+-- execute (Div    rg val)    (list, mem, reg, pc, labels) = print reg
+execute (Cmp    rg val)     (list, mem, reg, pc, labels) = cmp rg val (list, mem, reg, pc, labels)
+execute (Label  string)     (list, mem, reg, pc, labels) = nop (list, mem, reg, pc, labels)
+execute (Jmp    string)     (list, mem, reg, pc, labels) = jmp string (list, mem, reg, pc, labels)
+execute (Jc     string rg)  (list, mem, reg, pc, labels) = jc string rg (list, mem, reg, pc, labels)
+execute (Jeq    string rg)  (list, mem, reg, pc, labels) = jeq string rg (list, mem, reg, pc, labels)
 -- error in case of unmatched instruction
-execute x mem reg = Left (error ("Runtime Error: invalid statement '" ++ (show x) ++ "'"))
+execute instr (list, mem, reg, pc, labels) = Left (error ("Runtime Error: invalid statement '" ++ (show instr) ++ "'"))
 
 
 
@@ -88,26 +108,26 @@ execute x mem reg = Left (error ("Runtime Error: invalid statement '" ++ (show x
 
 
 
--- -- Instruction Functions
+-- Instruction Functions
 
 -- function that doesn't do anything and leaves the state of the SVM unchanged
-nop :: [Literal] -> [Literal] -> Either error ([Literal], [Literal])
-nop mem reg = Right (mem, reg)
+nop :: InstructionArgs -> Either error InstructionResult
+nop (list, mem, reg, pc, labels) = Right (list, mem, reg, pc, labels)
 
 -- function that copies the content or value of Arg2 into Arg1. If the memory address is out of range it will throw a runtime exception.
-mov :: Location -> Value -> [Literal] -> [Literal] -> Either error ([Literal], [Literal])
-mov (Address adr) (Location loc)   mem reg = case (locationRead loc mem reg) of
-                                              Left err -> Left err
-                                              Right value -> case (writeMem mem (addressValue adr reg) value) of
-                                                Left err -> Left err
-                                                Right rmem -> Right (rmem, reg)
-mov (Address adr) (Literal lit)    mem reg = case (writeMem mem (addressValue adr reg) (lit)) of
-                                              Left err -> Left err
-                                              Right rmem -> Right (rmem, reg)
-mov (Register rg) (Location loc)   mem reg = case (locationRead loc mem reg) of
-                                              Left err -> Left err
-                                              Right value -> Right (mem, (writeReg (rg) reg (value)))
-mov (Register rg) (Literal lit)    mem reg = Right (mem, (writeReg (rg) reg (lit)))
+mov :: Location -> Value -> InstructionArgs -> Either error InstructionResult
+mov (Address adr) (Location loc)   (list, mem, reg, pc, labels) = case (locationRead loc mem reg) of
+                                                                    Left err -> Left err
+                                                                    Right value -> case (writeMem mem (addressValue adr reg) value) of
+                                                                      Left err -> Left err
+                                                                      Right rmem -> Right (list, rmem, reg, (pc+1), labels)
+mov (Address adr) (Literal lit)    (list, mem, reg, pc, labels) = case (writeMem mem (addressValue adr reg) (lit)) of
+                                                                    Left err -> Left err
+                                                                    Right rmem -> Right (list, rmem, reg, (pc+1), labels)
+mov (Register rg) (Location loc)   (list, mem, reg, pc, labels) = case (locationRead loc mem reg) of
+                                                                    Left err -> Left err
+                                                                    Right value -> Right (list, mem, (writeReg (rg) reg (value)), (pc+1), labels)
+mov (Register rg) (Literal lit)    (list, mem, reg, pc, labels) = Right (list, mem, (writeReg (rg) reg (lit)), (pc+1), labels)
 
 -- -- function that stores 1 into Arg1 if both arguments are >= 0, otherwise - 1. It accepts only integer numbers, otherwise it raises a runtime exception.
 -- andI :: Register -> Either Register (Either Address constant) -> IO ()
@@ -133,10 +153,17 @@ mov (Register rg) (Literal lit)    mem reg = Right (mem, (writeReg (rg) reg (lit
 -- modI :: Register -> Either Register (Either Address constant) -> IO () -- change output from IO () to () to make it void instead of print
 -- modI arg1 arg2 = print (arg1 ++ arg2)
 --
--- -- function that computes the sum operation with Arg1 and Arg2 and stores the result in Arg1. It accepts only numerical arguments (inte- ger or float), otherwise it raises a runtime exception.
--- addI :: Register -> Either Register (Either Address constant) -> IO ()
--- addI arg1 arg2 = print (arg1 ++ arg2)
---
+-- function that computes the sum operation with Arg1 and Arg2 and stores the result in Arg1. It accepts only numerical arguments (integer or float), otherwise it raises a runtime exception.
+add :: Register -> Value -> InstructionArgs -> Either error InstructionResult
+add rg (Location loc) (list, mem, reg, pc, labels) = case (locationRead loc mem reg) of
+                                                      Left err -> Left err
+                                                      Right val -> case (addArgs (readReg rg reg) val) of
+                                                        Left err -> Left err
+                                                        Right sum -> Right (list, mem, (writeReg rg reg sum), (pc+1), labels)
+add rg (Literal val) (list, mem, reg, pc, labels)  = case (addArgs (readReg rg reg) val) of
+                                                      Left err -> Left err
+                                                      Right sum -> Right (list, mem, (writeReg rg reg sum), (pc+1), labels)
+
 -- -- function that computes the difference operation with Arg1 and Arg2 and stores the result in Arg1. It accepts only numerical arguments (integer or float), otherwise it raises a runtime exception.
 -- subI :: Register -> Either Register (Either Address constant) -> IO ()
 -- subI arg1 arg2 = print (arg1 ++ arg2)
@@ -148,26 +175,46 @@ mov (Register rg) (Literal lit)    mem reg = Right (mem, (writeReg (rg) reg (lit
 -- -- function that computes the division operation with Arg1 and Arg2 and stores the result in Arg1. Note that with integers you do the integer division and with floats the floating point division. It accepts only numerical arguments (integer or float), otherwise it raises a runtime exception.
 -- divI :: Register -> Either Register (Either Address constant) -> IO ()
 -- divI arg1 arg2 = print (arg1 ++ arg2)
---
--- -- function that compares the two arguments, returning -1 if Arg1 < Arg2, 0 if they are equal, 1 if Arg1 > Arg2. The result is stored in Arg1. It accepts only numerical values otherwise it raises a runtime exception.
--- cmpI :: Register -> Either Register (Either Address constant) -> IO ()
--- cmpI arg1 arg2 = print (arg1 ++ arg2)
---
--- --  function that jumps to the point of the program where the label with the name in Arg1 is defined. This instruction modifies the program counter to do so.
--- jmpI :: String -> IO ()
--- jmpI id = print id
---
--- --  function that jumps at the point of the program where the label with the name in Arg1 is defined, if Arg2 is >= 0. This instruction modifies the program counter to do so.
--- jcI :: String -> Register -> IO ()
--- jcI id arg2 = print (id ++ arg2)
---
+
+-- function that compares the two arguments, returning -1 if Arg1 < Arg2, 0 if they are equal, 1 if Arg1 > Arg2. The result is stored in Arg1. It accepts only numerical values otherwise it raises a runtime exception.
+cmp :: Register -> Value -> InstructionArgs -> Either error InstructionResult
+cmp rg (Location loc) (list, mem, reg, pc, labels) = case (locationRead loc mem reg) of
+                                                      Left err -> Left err
+                                                      Right val -> case (compareArgs (readReg rg reg) val) of
+                                                                    Left err -> Left err
+                                                                    Right result -> Right (list, mem, (writeReg rg reg result), (pc+1), labels)
+cmp rg (Literal val) (list, mem, reg, pc, labels)  = case (compareArgs (readReg rg reg) val) of
+                                                      Left err -> Left err
+                                                      Right result -> Right (list, mem, (writeReg rg reg result), (pc+1), labels)
+
+--  function that jumps to the point of the program where the label with the name in Arg1 is defined. This instruction modifies the program counter to do so.
+jmp :: String -> InstructionArgs -> Either error InstructionResult
+jmp string (list, mem, reg, pc, labels) = case (labelReturnRec string labels) of
+                                            Left err -> Left err
+                                            Right (rstring, rlist, rpc) -> Right (rlist, mem, reg, rpc, labels)
+
+--  function that jumps at the point of the program where the label with the name in Arg1 is defined, if Arg2 is >= 0. This instruction modifies the program counter to do so.
+jc :: String -> Register -> InstructionArgs -> Either error InstructionResult
+jc string rg (list, mem, reg, pc, labels) = case (compareArgs (readReg rg reg) (Integer (-1))) of
+                                              Left err -> Left err
+                                              Right result -> if (result == (Integer 1))
+                                                              then jmp string (list, mem, reg, (pc+1), labels)
+                                                              else Right (list, mem, reg, (pc+1), labels)
+
 -- --  function that jumps at the point of the program where the label with the name in Arg1 is defined, if Arg2 is 0. This instruction modifies the program counter to do so.
--- jeqI :: String -> Register -> IO ()
--- jeqI id arg2 = print (id ++ arg2)
---
--- --  operator that defines a label with the name specified in Arg1. It is not allowed to define labels with the same name, so in this case a runtime error should be raised.
--- labelI :: String -> IO ()
--- labelI id = print id
+jeq :: String -> Register -> InstructionArgs -> Either error InstructionResult
+jeq string rg (list, mem, reg, pc, labels) = case (compareArgs (readReg rg reg) (Integer 0)) of
+                                              Left err -> Left err
+                                              Right result -> if (result == (Integer 0))
+                                                              then jmp string (list, mem, reg, (pc+1), labels)
+                                                              else Right (list, mem, reg, (pc+1), labels)
+
+
+-- operator that defines a label with the name specified in Arg1. It is not allowed to define labels with the same name, so in this case a runtime error should be raised.
+label :: String -> InstructionArgs -> Either error InstructionResult
+label string (list, mem, reg, pc, labels)
+  | (labelExistsRec string labels) = Left (error ("Label " ++ string ++ " already defined.")) -- checks if the label exists
+  | otherwise = Right (list, mem, reg, (pc+1), labels ++ [(string, list, pc)]) -- adds the label to the list of labels
 
 
 
@@ -366,3 +413,45 @@ atIndex [] _ = Left (error "Index out of bounds")
 atIndex (x:xs) index
   | index <= 0 = Right x
   | otherwise = atIndex xs (index-1)
+
+-- recursive function for checking if a label exists
+labelExistsRec :: String -> Labels -> Bool
+labelExistsRec string [] = False
+labelExistsRec string ((lstring, x, _):list)
+  | (string == lstring) = True
+  | otherwise = labelExistsRec string list
+
+-- recursive function for returning the label values if it exists
+labelReturnRec :: String -> Labels -> Either error (String, [Instruction], Int)
+labelReturnRec string [] = Left (error (string ++ " is not defined as a label"))
+labelReturnRec string ((lstring, llist, pc):list)
+  | (string == lstring) = Right (string, llist, pc)
+  | otherwise = labelReturnRec string list
+
+-- function that compares two Literals, returning -1 if Arg1 < Arg2, 0 if they are equal, 1 if Arg1 > Arg2.
+compareArgs :: Literal -> Literal -> Either error Literal
+compareArgs (Integer arg1) (Integer arg2)
+  | (arg1 < arg2)  = Right (Integer (-1))
+  | (arg1 == arg2) = Right (Integer 0)
+  | (arg1 > arg2)  = Right (Integer 1)
+compareArgs (Integer arg1) (Float arg2)
+  | ((fromIntegral arg1 :: Double) < arg2)  = Right (Integer (-1))
+  | ((fromIntegral arg1 :: Double) == arg2) = Right (Integer 0)
+  | ((fromIntegral arg1 :: Double) > arg2)  = Right (Integer 1)
+compareArgs (Float arg1) (Integer arg2)
+  | (arg1 < (fromIntegral arg2 :: Double))  = Right (Integer (-1))
+  | (arg1 == (fromIntegral arg2 :: Double)) = Right (Integer 0)
+  | (arg1 > (fromIntegral arg2 :: Double))  = Right (Integer 1)
+compareArgs (Float arg1) (Float arg2)
+  | (arg1 < arg2)  = Right (Integer (-1))
+  | (arg1 == arg2) = Right (Integer 0)
+  | (arg1 > arg2)  = Right (Integer 1)
+compareArgs _ _ = Left (error ("Values are not numbers"))
+
+-- function for adding two Literals
+addArgs :: Literal -> Literal -> Either error Literal
+addArgs (Integer arg1) (Integer arg2) = Right (Integer (arg1 + arg2))
+addArgs (Integer arg1) (Float arg2) = Right (Float ((fromIntegral arg1 :: Double) + arg2))
+addArgs (Float arg1) (Integer arg2) = Right (Float (arg1 + (fromIntegral arg2 :: Double)))
+addArgs (Float arg1) (Float arg2) = Right (Float (arg1 + arg2))
+addArgs _ _ = Left (error ("Values are not numbers"))
